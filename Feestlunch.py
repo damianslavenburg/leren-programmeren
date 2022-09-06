7b121073a7db9e21279f8bd7dea515c9dd7d6c8e
damianslavenburg <99072083@mydavinci.nl>
2022-09-06 10:47:01 +0200

    Create Feestlunch.py

---
+++ Feestlunch.py
@@ -0,0 +1,10 @@
+#croisantjes
+# 17 stuks
+# stokbrood 2
+#2.78
+print (17 * 0.39)
+print (2 * 2.78)
+print (0.50 * 3)
+ 
+y = 17 * 0.39 + 2 * 2.78 -1.50 
+print ( "euro" + str(y)  + " " "nog te betalen")
