3afcc70bf0b00d235e371a782fae09584768b0a3
damianslavenburg <99072083@mydavinci.nl>
2022-09-06 12:28:49 +0200

    python

--- python/Feestlunch.py
+++ python/Feestlunch.py
@@ -1,14 +1,11 @@
 #croisantjes
 # stokbrood
 #2.78
-
 croisant = input("hoeveel croisant wil je: ")
 prijs_croisant = 0.39
 stokbrood = input("hoeveel stokbrooden wil je: ")
 prijs_stokbrood= 2.78
 kortingsbon = input("hoeveel kortingsbonnen heb je: ")
 kortingsbonnen_prijs = 0.50
-
 y = int(croisant) * prijs_croisant + int(stokbrood) * prijs_stokbrood - int(kortingsbon) * kortingsbonnen_prijs
-print (y)
-print("nog te betalen")
+print("de feestlunch kost bij de bakker ", y , "voor de " , croisant ," criosants en voor de ", stokbrood ," stokbroden als de ", kortingsbon ,"kortingsbonnen nog gelding zijn") 
--- python/speelhal.py
+++ python/speelhal.py
@@ -1,8 +1,6 @@
 tickets= 7.45 # ticket prijs persoon
 personen= input("hoeveel personen: ")
-print (tickets * int(personen) )
 vip= 0.074  #per minute
 min= input("hoeveel minuten wil je in de vip lounce: ")
-person_vip= input("hoeveel personen willen jullie in de vip")
-
-print( 'dat word dan',tickets * int(personen) + vip * int(min) * int(person_vip) )
+person_vip= input("hoeveel personen willen jullie in de vip: ")
+print('Dit geweldige dagje-uit met ',personen, " mensen in de speelhal met ", min , " minuten vr kost je maar ",tickets * int(personen) + vip * int(min) * int(person_vip) )
