fb7fd369feef602ee429bd9e779263f01caa356c
damianslavenburg <99072083@mydavinci.nl>
2022-09-06 12:06:13 +0200

    Update speelhal.py

--- python/speelhal.py
+++ python/speelhal.py
@@ -1,5 +1,8 @@
-tickets= 7.45
+tickets= 7.45 # ticket prijs persoon
 personen= input("hoeveel personen: ")
-print (tickets * personen)
-vip= 0.37 / 5
-min= input("hoeveel minuten wil je: ")
+print (tickets * int(personen) )
+vip= 0.074  #per minute
+min= input("hoeveel minuten wil je in de vip lounce: ")
+person_vip= input("hoeveel personen willen jullie in de vip")
+
+print( 'dat word dan',tickets * int(personen) + vip * int(min) * int(person_vip) )
