a269038fbb47b43f0dd9e1e82afcee1f10d33627
damianslavenburg <99072083@mydavinci.nl>
2022-09-06 14:02:35 +0200

    update

---
+++ pizzacalculator.py
@@ -0,0 +1,11 @@
+print("hallo welcome bij pizzaria mamazia wat is u naam:")
+naam= input()
+print("hallo", naam "hoe kan ik he helpen vandaag")
+print("wilt u een small, medium of large pizza?")
+pizza=input()
+#pizza prijsen
+small= 5
+medium=6.95
+large= 10
+pizza_size = input()
+print("hoeveel pizza wilt u")
