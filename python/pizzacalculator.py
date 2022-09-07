c15946b98d998878c37bf0c3560f8730a148611d
damianslavenburg <99072083@mydavinci.nl>
2022-09-07 12:14:35 +0200

    Update pizzacalculator.py

--- python/pizzacalculator.py
+++ python/pizzacalculator.py
@@ -1,9 +1,12 @@
 #damian slavenburg 99072083
 #naam en voorstellen
+from tkinter import N
+
+
 naam_res = "pizzaria mamaia"
-print("hallo welcome bij", naam_res , "wat is u naam?:")
+print("hallo welcome bij", naam_res , "wat is u naam?")
 naam = input()
-print("hallo" ,naam, "hoe kan ik je helpen vandaag?:")
+print("hallo" ,naam, "hoe kan ik je helpen vandaag?")
 #pizza menu
 pizza_menu = ["small{€5}", "medium{€6,95}", "large{€10}"]
 print(pizza_menu)
@@ -15,19 +18,18 @@ while repeat == True:
 
     if pizza_size == 'small':
         repeat = False
-        pizza_aantal= input('hoeveel pizzas wilt?:')
         prijs=5 
     elif pizza_size == 'medium':
         repeat = False
-        pizza_aantal= input('hoeveel pizzas wilt?:')
         prijs= 6,95
     elif pizza_size == 'large':
         repeat = False 
-        pizza_aantal= input('hoeveel pizzas wilt?:')
         prijs= 10
-
+    
     else:
         print(pizza_menu)
+
+pizza_aantal= input('hoeveel pizzas wilt u? ' '')
 #bonnetje
 totaal= int(pizza_aantal) * prijs  
 
