9414e17f1b560fd13f71e005a4940524b4a6b658
damianslavenburg <99072083@mydavinci.nl>
2022-09-07 11:35:40 +0200

    Update pizzacalculator.py

--- python/pizzacalculator.py
+++ python/pizzacalculator.py
@@ -1,11 +1,40 @@
-print("hallo welcome bij pizzaria mamazia wat is u naam:")
-naam= input()
-print("hallo", naam "hoe kan ik je helpen vandaag")
-print("wilt u een small, medium of large pizza?")
-pizza=input()
-#pizza prijsen
-small= 5
-medium=6.95
-large= 10
-pizza_size = input()
-print("hoeveel pizza wilt u")
+#naam en voorstellen
+naam_res = "pizzaria"
+print("hallo welcome bij", naam_res , "wat is u naam?:")
+naam = input()
+print("hallo" ,naam, "hoe kan ik je helpen vandaag?:")
+#pizza menu
+pizza_menu = ["small{€5}", "medium{€6,95}", "large{€10}"]
+print(pizza_menu)
+#pizza grote en aantal bestellen
+repeat = True
+
+while repeat == True:
+    pizza_size = input()
+
+    if pizza_size == 'small':
+        repeat = False
+        pizza_aantal= input('hoeveel pizzas wilt?:')
+        prijs=5 
+    elif pizza_size == 'medium':
+        repeat = False
+        pizza_aantal= input('hoeveel pizzas wilt?:')
+        prijs= 6,95
+    elif pizza_size == 'large':
+        repeat = False 
+        pizza_aantal= input('hoeveel pizzas wilt?:')
+        prijs= 10
+
+    else:
+        print(pizza_menu)
+#bonnetje
+totaal= int(pizza_aantal) * prijs  
+
+if int(pizza_aantal) == 1 :
+    print('---------------------------------')
+    print("u heeft " + str(pizza_aantal) + " " + str(pizza_size) + ' pizza besteld' )
+else:
+    print('---------------------------------')
+    print("u heeft " + str(pizza_aantal) + " " + str(pizza_size) + " pizza's besteld" )
+print( 'het totaal bedrag is €' ,totaal ,'danku wel voor het bestellen bij' , naam_res)
+print("---------------------------------")
