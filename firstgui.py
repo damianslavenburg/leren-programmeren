dfdd882ec3ea7b8ad7e4b3e21b8e73ed2c26bede
damianslavenburg <99072083@mydavinci.nl>
2022-09-07 20:10:37 +0200

    Create firstgui.py

---
+++ firstgui.py
@@ -0,0 +1,14 @@
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Text("hello from PySimleGUi")],
+    [sg.Button('ok')]
+]  
+window=sg.Window("demo", layout)
+
+while True:
+    event, values = window.read()
+    if event == "ok" or event == sg.WIN_CLOSED:
+        break
+window.close()
+    
