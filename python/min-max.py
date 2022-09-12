c11c4ad29dd00bd7716e0f042e0720ce6a343380
damianslavenburg <99072083@mydavinci.nl>
2022-09-12 09:44:09 +0200

    update

--- python/min-max.py
+++ python/min-max.py
@@ -4,14 +4,18 @@ print("voer het tweede getal in:")
 b= input()
  #1
 if a > b:
-    max= a
+    max = a
+    min = b
     print("a is het grootste getal:", max)
  #2
 elif a < b:
-    min=b
+    min = a
+    max = b 
     print("a is het kleinste getal:", min)
  #3
 else:
     print("a en b zijn even groot")
  #4
- 
+print("Het minimum is:", str(min))
+print("Het maximum is:", str(max))
+
