68b1de75c3bf69281b4308821f46d3354b3ee2b3
damianslavenburg <99072083@mydavinci.nl>
2022-09-12 09:30:26 +0200

    update

--- python/elif-statement.py
+++ python/elif-statement.py
@@ -2,9 +2,15 @@ print("voer het eerste getal in:")
 a= input()
 print("voer het tweede getal in:")
 b= input()
-if a < b:
+ #1
+if a > b:
     max= a
     print("a is het grootste getal:", max)
-
-
+ #2
+elif a < b:
+    min=b
+    print("a is het kleinste getal:", min)
+ #3
+else:
+    print("a en b zijn even groot")
     
