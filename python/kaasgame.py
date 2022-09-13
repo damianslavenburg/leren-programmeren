5e07cc89913dece51aabb7dd389a67247b4988dd
damianslavenburg <99072083@mydavinci.nl>
2022-09-13 10:47:56 +0200

    Update kaasgame.py

--- python/kaasgame.py
+++ python/kaasgame.py
@@ -7,57 +7,57 @@ while repeat == True:
     vraag = input().lower()
     
     if vraag == "ja":
-        repeat = True
+        repeat = False
         print("zitten er gaten in?")
         vraag=input().lower()
 
         if vraag == "ja":
-            repeat = True
+            repeat = False
             print("is de kaas belagelijk duur?")
             vraag = input().lower()
             if vraag == "ja":
-                repeat = True
+                repeat = False
                 print("dan is de kaas emmenthaler")
             elif vraag == "nee":
                 repeat = False
                 print("de kaas is dan leerdammer kaas") 
         
         elif vraag == "nee":
-            repeat = True
+            repeat = False
             print("is de kaas hard als een steen?")
             vraag = input().lower() 
             if vraag == "ja":
-                repeat = True
+                repeat = False
                 print("dan is de kaas parmigiano reggiano")
             if vraag == "nee":
-                repeat = True
+                repeat = False
                 print("dan is de kaas goudse kaas")
 
     elif vraag == "nee":
-        repeat = True
+        repeat = False
         print("heeft de kaas blauwe schimmel?")
         vraag=input().lower()
 
         if vraag == "ja":
-            repeat = True
+            repeat = False
             print("heeft de kaas een korst?")
             vraag = input().lower()
             if vraag == "ja":
-                repeat = True
+                repeat = False
                 print("dan is de kas blue de rochbaron")
             elif vraag == "nee":
-                repeat = True
+                repeat = False
                 print("dan is de kaas foume d'ambert") 
         
         elif vraag == "nee":
-            repeat = True
+            repeat = False
             print("heeft de kaas een korst?")
             vraag = input().lower() 
             if vraag == "ja":
-                repeat = True
+                repeat = False
                 print("dan is de kaas camembert")
             if vraag == "nee":
-                repeat = True
+                repeat = False
                 print("dan is de kaas mozzarella")
 else: 
     repeat = True
