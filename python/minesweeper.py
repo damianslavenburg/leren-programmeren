362032baf100bda24f8e4160c04ccd5a58f8f601
damianslavenburg <99072083@mydavinci.nl>
2022-09-26 13:18:30 +0200

    Update minesweeper.py

--- python/minesweeper.py
+++ python/minesweeper.py
@@ -6,23 +6,19 @@ while play == "yes" :
         vraag = print("choose a number between 1-10: ")
         number = input()
         trueOrFalse = random.randint(0,1)
-        trueOrFalse = 1
         bomb = 0
-        points = 0
-        point = True
+      
         if trueOrFalse:
-                point = True
                 print('there is no bomb here')
                 repeat = False
-        
-        if point == True:
                 print('you won')
+        
+      
 
         
         if not trueOrFalse:
                 repeat = False
                 print("bomb exploded")
-                point = False
         
         agian = input("do you want to play agian?")
         if agian == "yes": 
