5ae5ef8cadcbe3248369c4625cd80d36d31ae8b7
damianslavenburg <99072083@mydavinci.nl>
2022-09-28 09:18:02 +0200

    uodate

--- python/minesweeper.py
+++ python/minesweeper.py
@@ -1,29 +1,35 @@
 import random
-bomb = random.randint(1,10)
+bomb = random.randint(1,3)
 score = 0
 play = True
 ronde = 1
 while play == True:
         guess = input("u are at round " + str(ronde) + " choose random number: ")
-
-        if int(guess) == bomb:
-                play = False
-                print("game over bomb exploded")
         
-        elif int(guess) < 1: 
-                print('less than 1 is no possible')
-                play = True
-
-        elif int(guess) > 10 :
-                print('more than 10 is not possible')
-                play = True
-       
-        else: 
-                score = score + (ronde * ronde)
-                ronde = ronde + 1
-                print("u are safe")
-                nextronde = input('wilt u naar ronde '+ str(ronde) +'(y/n)')
-                play = True
-                if nextronde == "n":
+        try:
+                int(guess)
+                if int(guess) == bomb:
                         play = False
+                        print("game over bomb exploded")
+        
+                elif int(guess) < 1: 
+                        print('less than 1 is no possible')
+                        play = True
+
+                elif int(guess) > 10 :
+                        print('more than 10 is not possible')
+                        play = True
+                else: 
+                        score = score + (ronde * ronde)
+                        ronde = ronde + 1
+                        print("u are safe")
+                        nextronde = input('do you want to go to round '+ str(ronde) +'(y/n)')
+                        play = True
+                        if nextronde == "n":
+                                play = False
+        except:
+                print('that is not a number')
+
+               
 print('your score is', score)
+                
