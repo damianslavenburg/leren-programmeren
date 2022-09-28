293ca66ffc2d516b2a292ffbd63933da5c4c75ea
damianslavenburg <99072083@mydavinci.nl>
2022-09-28 10:02:00 +0200

    udpate

--- python/detijd.py
+++ python/detijd.py
@@ -2,8 +2,7 @@ from time import sleep
 
 for x in range (0,13):
     print(x , "am")
-    sleep(1)
+
 
 for x in range (13,24):
     print(x , "pm")
-    sleep(1)
---
+++ python/evengetallen.py
@@ -0,0 +1,3 @@
+
+for x in range (20,51,2):
+    print(x)
