4dca3f960243959351460fb5942c06429ee7f2bf
damianslavenburg <99072083@mydavinci.nl>
2022-09-28 10:24:40 +0200

    update

---
+++ python/robotarm-python-2021-main/example.py
@@ -0,0 +1,18 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 1')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.moveRight()
+
+robotArm.grab()
+
+robotArm.moveRight()
+
+robotArm.drop()
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
