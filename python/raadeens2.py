c23424ddaded3239289e64e997536df3dc51f163
damianslavenburg <99072083@mydavinci.nl>
2022-11-17 11:16:50 +0100

    update

--- python/raadeens2.py
+++ python/raadeens2.py
@@ -4,9 +4,51 @@ getal = random.randint(1,1000)
 keergeraden = 0
 ronde = 0
 repeat = True
-print(getal)
-raad = int(input("getal: "))
+score = 0
 while repeat == True:
+    print(getal)
+    raad = int(input("getal: "))
+    verschill = raad - getal
+    if abs(int(verschill)) <= 20:
+        print("je bent heel warm")
+        repeat = True
+    elif abs(int(verschill)) <= 50:
+        print("je bent warm")
+        repeat = True
+    if raad  < getal:
+            print("hoger")
+            keergeraden = keergeraden + 1
+            repeat = True
+    elif raad > getal:
+            print("lager")
+            keergeraden = keergeraden + 1
+            repeat = True
+        
+
+    if keergeraden == 10:
+        keergeraden = 0
+        opnieuw = input('je hebt teveel keer geraden wil je een nieuwgetal? ').lower()
+
+        if opnieuw == ("yes"): 
+            repeat = True
+            getal = random.randint(1,1000)
+        elif opnieuw == "no":
+            repeat = False 
+            quit
+
     if raad == getal:
-       print('geraden')
-    
+        print("dat is goed geraden")
+        ronde = ronde + 1
+        keergeraden = keergeraden - keergeraden
+        score = score + 1
+        print("u bent bij ronde",ronde)
+        nogeenronde = input("wilt u nog een ronde(yes of no)").lower()
+
+        if nogeenronde == "yes" or "ja":
+            getal = random.randint(1,1000)
+            repeat = True
+        
+        elif nogeenronde == "no" or "nee" or ronde == 20:
+            print('u heb',score,'punten')
+            repeat = False
+            quit
