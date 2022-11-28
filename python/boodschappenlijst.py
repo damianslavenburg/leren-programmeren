725b9befc49a716d6cd5ee48515dc05664b8ce2e
damianslavenburg <99072083@mydavinci.nl>
2022-11-28 14:30:21 +0100

    update

---
+++ python/boodschappenlijst.py
@@ -0,0 +1,28 @@
+lijst = {}
+repeat = True
+
+while repeat == True:
+    item = input("Wat wil je nog aan je boodschappen lijst toevoegen? ").lower()
+    aantal = int(input("Hoeveel wil je daarvan? "))
+
+    if item in lijst:
+            lijst[item] = lijst[item] + aantal
+            repeat = True
+
+    else:
+            lijst[item] = aantal
+
+    herhalen = input("Wilt u nog meer toevoegen?").lower()
+
+    if herhalen == "yes" or herhalen == "ja":
+        repeat = True
+
+    elif herhalen == "no" or herhalen == "nee":
+        repeat = False
+
+
+        print("\nHier is jou lijst:")
+        print("---------------------------")
+        for key, value in lijst.items():
+            print(value,"x ",key)
+        print("---------------------------")
