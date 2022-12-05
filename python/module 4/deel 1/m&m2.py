56f08fef4ff9f0ad0df088f0ff054baada645b40
damianslavenburg <99072083@mydavinci.nl>
2022-12-05 09:44:29 +0100

    up

--- python/module 4/deel 1/m&m2.py
+++ python/module 4/deel 1/m&m2.py
@@ -1,25 +1,16 @@
 import random
-oranje = 0
-blauw = 0
-groen = 0
-bruin = 0
+
 kleuren = ("oranje", "blauw", "groen", "bruin")
 zakvullen = {}
 aantal = int(input('hoeveel m&ms wil je?'))
 for x in range(aantal):
     choice = random.choice(kleuren)
-    if choice == "oranje":
-     oranje = oranje + 1
-     zakvullen.update({choice:oranje})
-    if choice == "blauw":
-        blauw = blauw + 1
-        zakvullen.update({choice:blauw})
-    if choice == "groen":
-        groen = groen + 1
-        zakvullen.update({choice:groen})
-    if choice == "bruin":
-        bruin = bruin + 1
-        zakvullen.update({choice:bruin})
+    if choice in zakvullen:
+            zakvullen[choice] = zakvullen[choice] + 1
+
+
+    else:
+            zakvullen[choice] = 1
 print(zakvullen)
 
 
--- python/module 4/deel 1/menm.py
+++ python/module 4/deel 1/menm.py
@@ -3,8 +3,7 @@ kleuren = ("oranje", "blauw", "groen", "bruin")
 zakvullen = []
 aantal = int(input('hoeveel m&ms wil je?'))
 for x in range(aantal):
-    choice = random.choice(kleuren)
-    zakvullen.append(choice)
+    zakvullen.append(random.choice(kleuren))
 print(zakvullen)
 
 
